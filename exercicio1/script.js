ac4fed2893ea89ac8f90406c1b4c3d891d520e6a
izabela08 <izabelalima17@gmail.com>
2022-08-14 18:07:52 -0300

    aula-013ii-izabela-lima

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,18 @@
-const num = //seu numero aqui
+const num = prompt("ESCREVA UM NUMERO: ")
+
+// 1. Utilizando ifs aninhados
+
+    if(num %2 == 0){
+        console.log("É DIVISIVEL POR 2")
+ if(num %1 == 0){
+        console.log("É DIVISIVEL POR 3")
+    }
+}
+  
+// 2. Utilizando um operador lógico
+
+    if(num %2 == 0 && num %1 == 0){ 
+        console.log('É DIVISIVEL POR 2 OU POR 3 ')
+
+     }
 
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,38 @@
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+// if(nacionalidade === "brasileira"){
+//     console.log("a pessoa é do Brasil!");
+// } else if(nacionalidade === "argentina"){
+//     console.log("a pessoa é da Argentina!");
+// } else if(nacionalidade === "uruguaia"){
+//     console.log("a pessoa é do Uruguai!");
+// } else if(nacionalidade === "chilena"){
+//     console.log("a pessoa é do Chile!");
+// } else if(nacionalidade === "colombiana"){
+//     console.log("a pessoa é da Colômbia!");
+// } else{
+//     console.log("nacionalidade não encontrada")
+// }
+
+// utilizando switch-case
+
+switch(nacionalidade){
+    case "brasileira":
+        console.log("NACIONALIDADE VALIDA")
+        break;
+     case "argentina":
+        console.log("NACIONALIDADE VALIDA")
+        break;
+    case "uruguaia":
+        console.log("NACIONALIDADE VALIDA")
+        break;
+    case "chilena":
+        console.log("NACIONALIDADE VALIDA")
+        break;
+    case "colambiana":
+        console.log("NACIONALIDADE VALIDA")
+        break;
+    default:
+        console.log('NACIONALIDADE NÃO VALIDA')
+
 }
